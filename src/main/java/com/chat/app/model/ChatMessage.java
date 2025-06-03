3d1da36efe23687125d9c405fdf0de5806470938
dayyan12k <salimdayyan@gmail.com>
2025-06-03 18:43:26 +0530

    Add files via upload

---
+++ src/main/java/com/chat/app/model/ChatMessage.java
@@ -0,0 +1,18 @@
+package com.chat.app.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ChatMessage {
+    private Long id;
+    private String sender;
+    private String content;
+    // getters and setters
+    public String getSender() { return sender; }
+    public void setSender(String sender) { this.sender = sender; }
+
+    public String getContent() { return content; }
+    public void setContent(String content) { this.content = content; }
+}
